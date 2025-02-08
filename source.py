1a947a1f672da505492415b10c5004b7c864a5aa
maxwong3 <42724546+maxwong3@users.noreply.github.com>
2025-02-08 17:52:53 -0500

    FINAL

--- source.py
+++ source.py
@@ -1,52 +1,37 @@
 from flask import Flask, render_template
-# from waitress import serve
+from waitress import serve
 import random
-import pandas as pd
+# import pandas as pd
+import csv
+app = Flask(__name__)
 
-# app = Flask(__name__)
-image = []
-product_name = []
-description = []
-price = []
-rating = []
-df = pd.read_csv('tide.csv')
-# print(df.dtypes)
+images = []
+product_names = []
+descriptions = []
+prices = []
+ratings = []
+retailers = []
+links = []
+with open('tide.csv', mode ='r')as file:
+    csvfile = csv.reader(file)
+    for lines in csvfile:
+        images.append(lines[6])
+        prices.append(lines[2])
+        product_names.append(lines[0])
+        descriptions.append(lines[4])
+        retailers.append(lines[3])
+        links.append(lines[5])
 
-#---------------------------------------------------------------Images
-df_image = df['Image']
 
-for i in range(len(df_image)):
-    image.append(df_image[i])
-#---------------------------------------------------------------ProductName
-df_product_name = df['Product Name']
+@app.route('/')
+@app.route('/main')
+def hello_world():
+    return render_template('main.html')
 
-for i in range(len(df_product_name)):
-    product_name.append(df_product_name[i])
-#---------------------------------------------------------------Description
-df_description = df['Description']
+@app.route('/nextproduct')
+def next_product():
+    rand_product = random.randint(0,31)
+    return render_template('product.html', price=prices[rand_product], image=images[rand_product], description=descriptions[rand_product], retailer=retailers[rand_product], name=product_names[rand_product], link=links[rand_product])
 
-for i in range(len(df_description)):
-    description.append(df_description[i])
-#---------------------------------------------------------------Price
-df_price = df['Cost']
-
-for i in range(len(df_price)):
-    price.append(df_price[i])
-#---------------------------------------------------------------Rating
-df_rating = df['Rating']
-
-for i in range(len(df_rating)):
-    rating.append(df_rating[i])
-#---------------------------------------------------------------All-For-Loops
-
-# @app.route('/')
-# def hello_world():
-#     return render_template('main.html')
-
-# @app.route('/nextproduct')
-# def next_product():
-#     rand_product = random.randint(0,2)
-#     return render_template('product.html', price=prices[rand_product], image=images[rand_product])
-
-# if __name__ == '__main__':
-#     serve(app, host="0.0.0.0", port=8000)
+if __name__ == '__main__':
+    serve(app, host="0.0.0.0", port=8000)
