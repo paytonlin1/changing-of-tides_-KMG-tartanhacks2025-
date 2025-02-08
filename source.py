77584f3da0f305d2777a12a192e1848c927f5d36
paytonlin1 <plinteq14@icloud.com>
2025-02-08 16:42:37 -0500

    uncommented comments

--- source.py
+++ source.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
-# from waitress import serve
+from waitress import serve
 import random
 import pandas as pd
 
-# app = Flask(__name__)
+app = Flask(__name__)
 image = []
 product_name = []
 description = []
@@ -39,14 +39,14 @@ for i in range(len(df_rating)):
     rating.append(df_rating[i])
 #---------------------------------------------------------------All-For-Loops
 
-# @app.route('/')
-# def hello_world():
-#     return render_template('main.html')
+@app.route('/')
+def hello_world():
+    return render_template('main.html')
 
-# @app.route('/nextproduct')
-# def next_product():
-#     rand_product = random.randint(0,2)
-#     return render_template('product.html', price=prices[rand_product], image=images[rand_product])
+@app.route('/nextproduct')
+def next_product():
+    rand_product = random.randint(0,2)
+    return render_template('product.html', price=prices[rand_product], image=images[rand_product])
 
-# if __name__ == '__main__':
-#     serve(app, host="0.0.0.0", port=8000)
+if __name__ == '__main__':
+    serve(app, host="0.0.0.0", port=8000)
