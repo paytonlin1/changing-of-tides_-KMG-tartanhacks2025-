e03592d6f40fc28512b034207831de4302695db3
maxwong3 <maxwon7@gmail.com>
2025-02-08 11:39:46 -0500

    initial flask implementation

--- source.py
+++ source.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('main.html')
+
+@app.route('/nextproduct')
+def next_product():
+    return render_template('product.html')
 
 if __name__ == '__main__':
     app.run()
